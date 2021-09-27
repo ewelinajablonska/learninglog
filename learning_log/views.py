b665061b0610f7c7a3082f41a91988ea77d7ff10
Ewelina <ewelina.jablonska.pl@gmail.com>
2021-09-27 13:38:45 +0200

    write a view for home page

--- learning_log/views.py
+++ learning_log/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    """The home page for the requesting logs"""
+    return render(request, 'learning_log/index.html')
